1b8420f4925567432881be8dc26c05db0747e361
jacocloeteHU <31765237+jacocloeteHU@users.noreply.github.com>
2020-04-30 16:47:29 +0200

    unit testing setup
    First unit testing setup added to gradle

--- src/test/java/lingo/words/WordsApplicationTests.java
+++ src/test/java/lingo/words/WordsApplicationTests.java
@@ -3,11 +3,27 @@ package lingo.words;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
 class WordsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testAdd() {
+	    assertEquals(42, Integer.sum(19, 23));
+	}
+	
+	@Test
+	public void testDivide() {
+	    assertThrows(ArithmeticException.class, () -> {
+	        Integer.divideUnsigned(42, 0);
+	    });
 	}
 
 }
