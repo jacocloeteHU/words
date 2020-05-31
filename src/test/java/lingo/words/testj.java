dbd463f4e4bf2fb1c618325d20e493e87c62e4dc
jacocloeteHU <31765237+jacocloeteHU@users.noreply.github.com>
2020-05-31 21:01:46 +0200

    architectuur verandering

---
+++ src/test/java/lingo/words/testj.java
@@ -0,0 +1,26 @@
+package lingo.words;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import lingo.words.application.factories.WordFactory;
+import lingo.words.application.filters.IFilter;
+import lingo.words.application.filters.WordFilter;
+import lingo.words.application.services.WordService;
+import lingo.words.domain.model.Word;
+import lingo.words.domain.services.DomainServices;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+
+public class testj {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
