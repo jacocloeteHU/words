322de65b808b6c5589cfe330e9b0088490f825e7
jacocloeteHU <31765237+jacocloeteHU@users.noreply.github.com>
2020-06-18 14:11:09 +0200

    add

--- src/main/java/lingo/words/infrastructure/serialization/CsvWordDeserializer.java
+++ src/main/java/lingo/words/infrastructure/serialization/CsvWordDeserializer.java
@@ -7,14 +7,19 @@ public class CsvWordDeserializer implements IWordDeserializer {
 
 	@Override
 	public ArrayList<String> deserialize(String data) {
-		ArrayList<String> wordsList = new ArrayList<String>();
-		Scanner scanner = new Scanner(data);
-		while (scanner.hasNextLine()) {
-		  String line = scanner.nextLine();
-		  wordsList.add(line);
+		try {
+			ArrayList<String> wordsList = new ArrayList<String>();
+			Scanner scanner = new Scanner(data);
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				wordsList.add(line);
+			}
+			scanner.close();
+
+			return wordsList;
+		} catch (Exception e) {
+			throw e;
 		}
-		scanner.close();
-		return wordsList;
 	}
 
 }
